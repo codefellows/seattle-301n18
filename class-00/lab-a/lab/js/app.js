51f3bb9077f95dc7c72310e115cedf76c9ddec13
Ryan Geddes <rgeddes09@gmail.com>
2020-05-30 14:13:03 -0700

    changed variables to let

---
+++ class-00/lab-a/lab/js/app.js
@@ -0,0 +1,137 @@
+'use strict';
+
+let names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+let leftImage = document.getElementById('left');
+let centerImage = document.getElementById('center');
+let rightImage = document.getElementById('right');
+
+let allProducts = [];
+let container = document.getElementById('image_container');
+let viewed = [];
+let labels = [];
+let pics = [leftImage, centerImage, rightImage];
+let list = document.getElementById('productlist');
+let totalClicks = 0;
+let views = [];
+let votes = [];
+
+function Product(name) {
+  this.name = name;
+  this.path = 'img/' + name + '.jpg';
+  this.votes = 0;
+  this.views = 0;
+  allProducts.push(this);
+}
+
+function makeRandom() {
+  return Math.floor(Math.random() * names.length);
+}
+
+function displayPics(){
+  while(viewed.length < 6){
+    let rando = makeRandom();
+    while(!viewed.includes(rando)){
+      viewed.push(rando);
+    }
+  }
+  console.log(rando);
+  // TODO: In a sentence or two, explain why the previous line of code threw an error 
+  //when we changed the letiable declaration from `let to `let`.
+  // PUT YOUR RESPONSE IN THIS COMMENT
+  console.log(viewed);
+
+  for (let i = 0; i < 3; i++){
+    let temp = viewed.shift();
+    pics[i].src = allProducts[temp].path;
+    pics[i].id = allProducts[temp].name;
+    allProducts[temp].views += 1;
+  }
+}
+
+function handleClick(event) {
+  if (event.target.id === 'image_container') {
+    return alert('Be sure to click directly on an image!!');
+  }
+  totalClicks += 1;
+  if(totalClicks > 24) {
+    container.removeEventListener('click', handleClick);
+    container.style.display = 'none';
+    showList();
+    makeChart();
+  }
+  for(let i = 0; i < names.length; i++){
+    if(event.target.id === allProducts[i].name) {
+      allProducts[i].votes += 1;
+      console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+    }
+  }
+  localStorage.busmall = JSON.stringify(allProducts);
+  localStorage.busmallProducts = JSON.stringify(allProducts);
+  displayPics();
+}
+
+function showList() {
+  for(let i = 0; i < allProducts.length; i++) {
+    let liEl = document.createElement('li');
+    liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+    list.appendChild(liEl);
+  }
+}
+
+function makeChartData(){
+  allProducts.forEach(function(product){
+    labels.push(product.name);
+    votes.push(product.votes);
+    views.push(product.views);
+  });
+}
+
+function makeChart(){
+  makeChartData();
+  let ctx = document.getElementById('chartypants').getContext('2d');
+  new Chart(ctx, { //eslint-disable-line
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'total votes',
+        backgroundColor: 'gold',
+        borderColor: '#214',
+        data: votes,
+      }]
+    },
+    options: {
+      responsive: false,
+      scales: {
+        yAxes: [{
+          ticks: {
+            max: 20,
+            min: 0,
+            stepSize: 1
+          }
+        }]
+      }
+    }
+  });
+  Chart.defaults.global.defaultFontColor = '#eee'; //eslint-disable-line
+}
+
+container.addEventListener('click', handleClick);
+
+document.getElementById('bus').addEventListener('click', function(){
+  localStorage.removeItem('busmall');
+  console.log('Local storage was cleared!');
+});
+
+if(localStorage.busmall){
+  console.log('Local storage data exists');
+  allProducts = JSON.parse(localStorage.busmall);
+} else {
+  console.log('There is no local storage data; initialize app by creating instances');
+  for(let i = 0; i < names.length; i++) {
+    new Product(names[i]);
+  }
+}
+
+displayPics();
