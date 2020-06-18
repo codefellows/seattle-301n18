b5e0625721bf1251d57adf147004c96db6cfee73
John Cokos <highlander.44@gmail.com>
2020-06-17 20:41:42 -0700

    Updated day 06

--- class-06/demo/in-class/server/server.js
+++ class-06/demo/in-class/server/server.js
@@ -1,5 +1,65 @@
 'use strict';
 
+// dotenv, express, cors
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+// this is MAGIC. Trust
+// Anything from the .env file shows up here
+const PORT = process.env.PORT;
+
+// Get an "instance" of express as our app
+const app = express();
+
+app.use( cors() );
+
+app.get('/location', (request,response) => {
+  // Read in data that came from an external API
+  let data = require('./data/location.json');
+  // Adapt the data to match the contract
+  let actualData = new Location(data[0]);
+  // Send out the adapted data
+  response.status(200).json(actualData);
+});
+
+function Location( obj ) {
+  this.latitude = obj.lat;
+  this.longitude = obj.lon;
+  this.formatted_query = obj.display_name;
+}
+
+// $('thing').on('something', () => {})
+app.get('/restaurants', (request, response) => {
+  let data = require('./data/restaurants.json');
+
+  let allRestaurants = [];
+  data.nearby_restaurants.forEach( restObject => {
+    let restaurant = new Restaurant(restObject);
+    allRestaurants.push(restaurant);
+  });
+
+  response.status(200).json(allRestaurants);
+});
+
+function Restaurant(obj) {
+  this.restaurant = obj.restaurant.name;
+  this.locality = obj.restaurant.location.locality;
+  this.cuisines = obj.restaurant.cuisines;
+}
+
+// app.put(), app.delete(), app.post()
+
+app.use('*', (request,response) => {
+  response.status(404).send('Huh?');
+});
+
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.status(500).send('server is broken');
+});
+
+app.listen( PORT, () => console.log('Server running on port', PORT));
 
 // Handle a request for location data
 // Get a city from the client
--- class-06/demo/in-class/website/app.js
+++ class-06/demo/in-class/website/app.js
@@ -1,27 +1,64 @@
 'use strict';
 
-
 // Event Listener on the search button
-// $('form').on('submit', getLocation)
+$('form').on('submit', getLocation);
+
+function getLocation(e) {
+  e.preventDefault();
+
+  // 1. Get the word (city) the person typed?
+  let city = $('#city-name').val();
+  console.log('you searched for', city);
+
+  // 2. $.ajax() to our server, asking for location, sending the city
+  //    http://localhost:3000/location?city=seattle
+  //    This will return some data... (lat,long,city name)
+  $.ajax(`http://localhost:3000/location?city=${city}`)
+    .then( locationData => {
+      console.log('From the server', locationData);
+      // 3. Render the map  renderMap(lat,long)
+      renderMap(locationData);
+      renderHeading(locationData);
+      // 4. Get Restuarant Data
+      getRestaurants(locationData);
+    });
+
+}
 
 
-// getLocation()
-// 1. Get the word (city) the person typed?
-//    - jQuery input field .val()
-// 2. $.ajax() to our server, asking for location, sending the city
-//    http://localhost:3000/location?city=seattle
-//    This will return some data... (lat,long,city name)
-// 3. Render the map  renderMap(lat,long)
-// 4. Get Restuarant Data
-//    call getResturants(lat,long)
+function renderMap(location) {
+  let template = $('#map-template').html();
+  let mapHTML = Mustache.render(template, location);
+  $('#map').html(mapHTML);
+}
 
+function renderHeading(location) {
+  let template = $('#heading-template').html();
+  let headingHTML = Mustache.render(template, location);
+  $('#heading').html(headingHTML);
+}
 
-// renderMap(lat,long)
-// Takes lat/long and draws the actual map
+function getRestaurants(location) {
+  // $.ajax() to our server, asking for restaurants, sending lat,long
+  // render them as a list, using the renderRestaurants()
+  $.ajax('http://localhost:3000/restaurants')
+    .then( restaurants => {
+      console.log(restaurants);
+      renderRestaurants(restaurants);
+    });
+}
 
-// getRestaurants(lat,long)
-// $.ajax() to our server, asking for restaurants, sending lat,long
-// render them as a list, using the renderRestaurants()
+function renderRestaurants(restaurants) {
 
-// renderRestaurants()
-// Takes a list, renders them with mustache
+  // Takes a list, renders them with mustache
+  let $template = $('#restaurants-template').html();
+  let $target = $('#restaurants');
+
+  $target.empty();
+
+  restaurants.forEach( (r) => {
+    let rHTML = Mustache.render( $template, r );
+    $target.append(rHTML);
+  });
+
+}
